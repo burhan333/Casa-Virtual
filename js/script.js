7a9bc36edfe01ae54589dc7cde06d418e2cae8c7
burhan <em.burhan@yahoo.com>
2020-05-16 02:08:30 +0500

    Casa Virtual

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const popup = document.getElementById('filtro')
+
+function show()
+{
+    popup.style.display = 'block';
+}
+
+function hide()
+{
+    popup.style.display = 'none';
+    console.log('click')
+}
